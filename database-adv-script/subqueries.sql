74878dcc34f11dd251551d60737f7becda12fd3a
Sama Mostafa <231562182+SamaMostafa-Programmer@users.noreply.github.com>
2025-11-02 06:12:47 +0200

    Add subquery examples to subqueries.sql
    Added examples of non-correlated and correlated subqueries.

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,15 @@
+-- Non-correlated subquery
+SELECT name
+FROM properties
+WHERE id IN (
+  SELECT property_id FROM reviews
+  GROUP BY property_id
+  HAVING AVG(rating) > 4.0
+);
+
+-- Correlated subquery
+SELECT name
+FROM users u
+WHERE (
+  SELECT COUNT(*) FROM bookings b WHERE b.user_id = u.id
+) > 3;

08aa947a4660fe8620980dcd3fb1e6511bb8f3cf
Sama Mostafa <231562182+SamaMostafa-Programmer@users.noreply.github.com>
2025-11-02 07:49:31 +0200

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -4,14 +4,9 @@ FROM users u
 INNER JOIN bookings b ON u.id = b.user_id;
 
 -- LEFT JOIN
--- LEFT JOIN: Retrieve all properties and their reviews, including properties with no reviews
-SELECT 
-    p.id AS property_id, 
-    p.name AS property_name, 
-    r.rating AS review_rating
+SELECT p.id AS property_id, p.name AS property_name, r.rating AS review_rating
 FROM properties p
-LEFT JOIN reviews r 
-    ON p.id = r.property_id;
+LEFT JOIN reviews r ON p.id = r.property_id;
 
 -- FULL OUTER JOIN (PostgreSQL syntax)
 SELECT u.id AS user_id, b.id AS booking_id
